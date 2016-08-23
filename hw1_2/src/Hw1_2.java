a5ec4ba90e5530737b11cbf04f67c25d3fcff17f
Kasididh <buwinkasididh@hotmail.com>
2016-08-23 15:17:34 +0700

    add some function

--- hw1_2/src/Hw1_2.java
+++ hw1_2/src/Hw1_2.java
@@ -4,10 +4,15 @@
 public class Hw1_2 {
     float gpaCalculation(int[] credit, int[] grade)
     {
-        /**
-         * your code here
-         */
-        return 0.0f;
+        float score=0;
+        int allcredit=0;
+        for(int i=0;i<credit.length;i++){
+            score=score+credit[i]*grade[i];
+            allcredit=allcredit+credit[i];
+        }
+        float   allgrede=0;
+        allgrede=score/allcredit;
+        return allgrede;
     }
 
     public static void main(String[] args) {
