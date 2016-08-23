4dd092519f9393e8e8187fe9fe435827d86d5fb4
Kantapith Muangjai <artpith@gmail.com>
2016-08-23 15:16:08 +0700

    add code

--- hw1_2/src/Hw1_2.java
+++ hw1_2/src/Hw1_2.java
@@ -4,16 +4,20 @@
 public class Hw1_2 {
     float gpaCalculation(int[] credit, int[] grade)
     {
-        /**
-         * your code here
-         */
-        return 0.0f;
+        float gpa=0;
+        int i,all=0;
+        for (i=0;i<credit.length;i++) {
+            all += credit[i];
+            gpa += (grade[i] * credit[i]);
+        }
+        return (float)  gpa/all;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int[] credit = {3, 3, 3, 3, 2};
         int[] grade  = {2, 4, 4, 3, 2};
         Hw1_2 h = new Hw1_2();
-        System.out.printf("Your GPA is %f\n", h.gpaCalculation(credit, grade));
+        System.out.printf("Your GPA is %.2f\n", h.gpaCalculation(credit, grade));
     }
 }
