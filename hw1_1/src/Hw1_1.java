9ed3a8122be2c37b5f45f7ce24a987f030b51cb2
pattakorn_p <pattakorn_p@cmu.ac.th>
2016-08-23 15:15:08 +0700

    add code

--- hw1_1/src/Hw1_1.java
+++ hw1_1/src/Hw1_1.java
@@ -4,10 +4,11 @@
 public class Hw1_1 {
     public int countEvenNum(int[] inputs)
     {
-        /**
-         * Your code here
-         */
-        return 0;
+        int eCount = 0;
+        for(int i = 0;i < inputs.length;i++){
+            if (inputs[i] % 2 == 0) eCount++;
+        }
+        return eCount;
     }
 
 
